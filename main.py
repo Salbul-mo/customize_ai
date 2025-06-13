4e36a423908530816bf4ffc51a2938e3c4a734c9
Salbul-mo <salbul.mo91@gmail.com>
2025-06-13 22:14:45 +0900

    [fix] LLM 이 실행할 함수를 판단하면 main.py 에서 직접 실행하도록 기능 추가 및 루프를 통해 연속적인 작업을 할 수 있게 추가가

--- main.py
+++ main.py
@@ -3,6 +3,127 @@ import sys
 from dotenv import load_dotenv
 from google import genai
 from google.genai import types
+from functions.get_file_content import get_file_content
+from functions.get_files_info import get_files_info
+from functions.run_python_file import run_python_file
+from functions.write_file import write_file 
+
+system_prompt = """
+You are a helpful AI coding agent.
+
+When a user asks a question or makes a request, make a function call plan. You can perform the following operations:
+
+- List files and directories
+- Read file contents
+- Execute Python files with optional arguments
+- Write or overwrite files
+
+All paths you provide should be relative to the working directory. You do not need to specify the working directory in your function calls as it is automatically injected for security reasons.
+"""
+
+schema_get_files_info = types.FunctionDeclaration(
+    name="get_files_info",
+    description="Lists files in the specified directory along with their sizes, constrained to the working directory.",
+    parameters=types.Schema(
+        type=types.Type.OBJECT,
+        properties={
+            "directory": types.Schema(
+                type=types.Type.STRING,
+                description="The directory to list files from, relative to the working directory. If not provided, lists files in the working directory itself.",
+            ),
+        },
+    ),
+)
+
+schema_get_file_content = types.FunctionDeclaration(
+    name="get_file_content",
+    description="Reads the content of a file, constrained to the working directory.",
+    parameters=types.Schema(
+        type=types.Type.OBJECT,
+        properties={
+            "file_path": types.Schema(
+                type=types.Type.STRING,
+                description="The path to the file to read, relative to the working directory.",
+            ),
+        },
+    ),
+)
+
+schema_run_python_file = types.FunctionDeclaration(
+    name="run_python_file",
+    description="Runs a Python file, constrained to the working directory.",
+    parameters=types.Schema(
+        type=types.Type.OBJECT,
+        properties={
+            "file_path": types.Schema(
+                type=types.Type.STRING,
+                description="The path to the Python file to run, relative to the working directory.",
+            ),
+        },
+    ),
+)
+
+schema_write_file = types.FunctionDeclaration(
+    name="write_file",
+    description="Writes content to a file, constrained to the working directory.",
+    parameters=types.Schema(
+        type=types.Type.OBJECT,
+        properties={
+            "file_path": types.Schema(
+                type=types.Type.STRING,
+                description="The path to the file to write, relative to the working directory.",
+            ),
+            "content": types.Schema(
+                type=types.Type.STRING,
+                description="The content to be written to the file.",
+            ),
+        },
+    ),
+)
+
+available_functions = types.Tool(
+    function_declarations=[
+        schema_get_files_info,
+        schema_get_file_content,
+        schema_run_python_file,
+        schema_write_file,
+    ]
+)
+
+
+def call_function(function_call_part, verbose=False):
+    function_name = function_call_part.name
+    try:
+        if verbose:
+            print(
+                f"Calling function: {function_call_part.name}({function_call_part.args})"
+            )
+        else:
+            print(f" - Calling function: {function_call_part.name}")
+
+        new_args = {"working_directory": "./calculator", **function_call_part.args}
+
+        function_result = globals()[function_name](**new_args)
+
+        return types.Content(
+            role="tool",
+            parts=[
+                types.Part.from_function_response(
+                    name=function_name, response={"result": function_result}
+                )
+            ],
+        )
+
+    except Exception as e:
+        return types.Content(
+            role="tool",
+            parts=[
+                types.Part.from_function_response(
+                    name=function_name,
+                    response={"error": f"Unknown function: {function_name}"},
+                )
+            ],
+        )
 
 
 load_dotenv()
@@ -12,30 +133,59 @@ client = genai.Client(api_key=api_key)
 
 user_prompt = " ".join(sys.argv[1:])
 
+llm_loop_cnt = 0
+
+    
 # sys.argv[0] => 파일 이름
-messages = [
-    types.Content(role="user", parts=[types.Part(text=user_prompt)])
-]
+messages = [types.Content(role="user", parts=[types.Part(text=user_prompt)])]
+verbose = False
 
 # print(messages)
 # print(",".join(sys.argv[1:]))
 
-try:
-    if messages:
-        response = client.models.generate_content(
-            model="gemini-2.0-flash-001",
-            contents=messages,
-        )
-        if "--verbose" in user_prompt:
-            print(f"User prompt: {user_prompt}")
-            print(f"Prompt tokens: {response.usage_metadata.prompt_token_count}")
-            print(f"Response tokens: {response.usage_metadata.candidates_token_count}")
-            print(f"Response: {response.text}")
+while llm_loop_cnt < 20:
+    try:
+        if messages:
+            response = client.models.generate_content(
+             model="gemini-2.0-flash-001",
+             contents=messages,
+             config=types.GenerateContentConfig(
+                 tools=[available_functions], system_instruction=system_prompt
+                ),
+            )
+            for candidate in response.candidates:
+                messages.append(
+                    candidate.content
+                )
+            if "--verbose" in user_prompt:
+                verbose = True
+                print(f"User prompt: {user_prompt}")
+                print(f"Prompt tokens: {response.usage_metadata.prompt_token_count}")
+                print(f"Response tokens: {response.usage_metadata.candidates_token_count}")
+
+            if response.function_calls:
+                for function_call in response.function_calls:
+                    response_result = call_function(function_call, verbose)
+                    messages.append(response_result)
+                    if not response_result.parts[0].function_response.response:
+                        raise Exception("fatal exception while call_function")
+                    else:
+                        if verbose:
+                            print(
+                                f"-> {response_result.parts[0].function_response.response}"
+                            )
+
+            else:
+                break
+                
+            llm_loop_cnt += 1
+
+        
         else:
-            print(f"{response.text}")
-except:
-    print("There is no prompt input")
-    sys.exit(1)
+            print("There is no prompt input")
+            sys.exit(1)
+    except Exception as e:
+        print(f"Error: {e}")
+        sys.exit(1)
 
-
-
+print(f"{response.text}")
